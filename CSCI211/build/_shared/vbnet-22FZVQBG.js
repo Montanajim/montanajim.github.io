218f8543a7fdde6e55b5b3c7f3298146e9c57fe6
Jim Goudy <8660573+Montanajim@users.noreply.github.com>
2026-04-09 14:51:33 -0600

    updated base url
    forgot to update base url

--- CSCI211/build/_shared/vbnet-22FZVQBG.js
+++ CSCI211/build/_shared/vbnet-22FZVQBG.js
@@ -1,1 +1,1 @@
-import{a}from"/build/_shared/chunk-C2LAERNE.js";import"/build/_shared/chunk-TEESLJ7F.js";import"/build/_shared/chunk-OZE3FFNP.js";export default a();
+import{a}from"/CSCI211/build/_shared/chunk-C2LAERNE.js";import"/CSCI211/build/_shared/chunk-TEESLJ7F.js";import"/CSCI211/build/_shared/chunk-OZE3FFNP.js";export default a();
